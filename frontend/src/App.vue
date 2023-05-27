e61874192bc2538657ca661d79047b784153e79e
OrdenKrieger <OrdenKriegerEDR@googlemail.com>
2023-05-27 10:32:12 +0200

    web: Add base for a login view

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,29 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import {
+  NConfigProvider,
+  NMessageProvider,
+  NDialogProvider,
+  darkTheme,
+  type GlobalThemeOverrides
+} from 'naive-ui'
+
+const themeOverrides: GlobalThemeOverrides = {
+  common: {
+    primaryColor: '#a855f7', // Purple 500
+    primaryColorHover: '#d8b4fe', // Purple 300
+    primaryColorPressed: '#e9d5ff', // Purple 200
+    borderRadius: '0.4rem',
+  }
+}
 </script>
 
 <template>
-  <RouterView />
+  <n-config-provider :theme-overrides="themeOverrides" :theme="darkTheme">
+    <NDialogProvider>
+      <n-message-provider>
+        <RouterView />
+      </n-message-provider>
+    </NDialogProvider>
+  </n-config-provider>
 </template>
---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import {
+  NForm,
+  NFormItem,
+  NInput,
+  NButton,
+  type FormInst,
+  type FormRules,
+  type FormValidationError
+} from 'naive-ui'
+
+const formRef = ref<FormInst | null>(null)
+
+interface LoginModel {
+  email: string
+  password: string
+}
+
+const model = ref<LoginModel>({
+  email: '',
+  password: ''
+})
+
+const rules: FormRules = {
+  email: [
+    {
+      required: true,
+      message: 'E-Mail is required'
+    }
+  ],
+  password: [
+    {
+      required: true,
+      message: 'Password is required'
+    }
+  ]
+}
+
+function handleLogin() {
+  formRef.value?.validate((errors: Array<FormValidationError> | undefined) => {
+    if (!errors) {
+      //
+    } else {
+      //
+    }
+  })
+}
+</script>
+
+<template>
+  <div class="flex justify-center items-center h-screen bg-gray-950">
+    <div class="flex flex-col gap-4 items-center w-96 p-4 rounded-xl bg-slate-900">
+      <span
+        class="text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-indigo-100 to-purple-500"
+      >
+        Home Hub</span
+      >
+      <n-form ref="formRef" :model="model" :rules="rules" class="w-full">
+        <n-form-item path="email" label="E-Mail" :show-require-mark="false">
+          <n-input v-model:value="model.email" placeholder="" />
+        </n-form-item>
+        <n-form-item path="password" label="Password" :show-require-mark="false">
+          <n-input v-model:value="model.password" type="password" placeholder="" />
+        </n-form-item>
+        <n-button
+          class="w-full"
+          :disabled="model.email === '' || model.password === ''"
+          type="primary"
+          strong
+          secondary
+          @click="handleLogin"
+        >
+          Login
+        </n-button>
+      </n-form>
+    </div>
+  </div>
+</template>
