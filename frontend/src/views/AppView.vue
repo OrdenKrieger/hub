9f2ff830185372941aea3622f09d2c21c191cbc5
OrdenKrieger <OrdenKriegerEDR@googlemail.com>
2023-05-28 20:19:39 +0200

    web: Add mobile menu

--- frontend/src/views/AppView.vue
+++ frontend/src/views/AppView.vue
@@ -3,10 +3,15 @@ import { ref, computed } from 'vue'
 import { RouterView, useRouter } from 'vue-router'
 import { NMenu, NLayout, NLayoutSider, type MenuOption } from 'naive-ui'
 import { renderIcon, routerLink } from '@/utils/naive'
+import DropdownIcon from '@/components/icons/DropdownIcon.vue'
 import DashboardIcon from '@/components/icons/DashboardIcon.vue'
 import SettingsIcon from '@/components/icons/SettingsIcon.vue'
 
 const router = useRouter()
+
+// Todo mobile trigger
+const mobile = false
+const showMobile = ref(false)
 
 const showFullApp = computed(() => {
   switch (router.currentRoute.value.name) {
@@ -40,9 +45,25 @@ const menuOptions: MenuOption[] = [
 </script>
 
 <template>
-  <n-layout v-if="showFullApp" has-sider>
+  <n-layout v-if="showFullApp" :has-sider="!mobile">
+    <div v-if="mobile" class="bg-gray-950">
+      <div class="flex justify-end p-1.5 pr-2">
+        <DropdownIcon
+          @click="showMobile = !showMobile"
+          class="h-10 fill-purple-600 bg-purple-600 bg-opacity-20 rounded-md cursor-pointer"
+        ></DropdownIcon>
+      </div>
+      <n-layout v-if="showMobile" class="bg-gray-950">
+        <n-menu
+          v-model:value="currentKey"
+          :collapsed-width="64"
+          :collapsed-icon-size="22"
+          :options="menuOptions"
+        />
+      </n-layout>
+    </div>
     <n-layout-sider
-      bordered
+      v-else
       collapse-mode="width"
       :collapsed-width="64"
       :width="240"
