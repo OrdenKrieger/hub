ede15a298c806ae0dbd707510187287c516e581e
OrdenKrieger <OrdenKriegerEDR@googlemail.com>
2023-05-28 19:31:32 +0200

    web: Eye candy for login page

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -50,30 +50,37 @@ function handleLogin() {
 
 <template>
   <div class="flex justify-center items-center h-screen bg-gray-950">
-    <div class="flex flex-col gap-4 items-center w-96 p-4 rounded-xl bg-slate-900">
-      <span
-        class="text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-indigo-100 to-purple-500"
+    <div class="relative">
+      <div
+        class="absolute inset-0.5 bg-gradient-to-tl from-purple-600 to-indigo-100 blur-md opacity-80"
+      ></div>
+      <div
+        class="relative flex flex-col gap-4 items-center w-64 sm:w-96 p-4 rounded-xl bg-slate-900"
       >
-        Home Hub</span
-      >
-      <n-form ref="formRef" :model="model" :rules="rules" class="w-full">
-        <n-form-item path="email" label="E-Mail" :show-require-mark="false">
-          <n-input v-model:value="model.email" placeholder="" />
-        </n-form-item>
-        <n-form-item path="password" label="Password" :show-require-mark="false">
-          <n-input v-model:value="model.password" type="password" placeholder="" />
-        </n-form-item>
-        <n-button
-          class="w-full"
-          :disabled="model.email === '' || model.password === ''"
-          type="primary"
-          strong
-          secondary
-          @click="handleLogin"
+        <span
+          class="text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-indigo-100 to-purple-500"
         >
-          Login
-        </n-button>
-      </n-form>
+          Home Hub</span
+        >
+        <n-form ref="formRef" :model="model" :rules="rules" class="w-full">
+          <n-form-item path="email" label="E-Mail" :show-require-mark="false">
+            <n-input v-model:value="model.email" placeholder="" />
+          </n-form-item>
+          <n-form-item path="password" label="Password" :show-require-mark="false">
+            <n-input v-model:value="model.password" type="password" placeholder="" />
+          </n-form-item>
+          <n-button
+            class="w-full"
+            :disabled="model.email === '' || model.password === ''"
+            type="primary"
+            strong
+            secondary
+            @click="handleLogin"
+          >
+            Login
+          </n-button>
+        </n-form>
+      </div>
     </div>
   </div>
 </template>
