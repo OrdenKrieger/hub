2f4de549fc8178685dd027bd1650a095e7fff0d3
OrdenKrieger <OrdenKriegerEDR@googlemail.com>
2024-11-24 00:43:56 +0100

    api: Add OAuth2 social authentication
    * Github for testing rigth now

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -14,3 +14,44 @@ router.get('/', async () => {
     hello: 'world',
   }
 })
+
+router
+  .get('/:provider/redirect', ({ ally, params }) => {
+    return ally.use(params.provider).redirect()
+  })
+  .where('provider', /github/)
+
+router
+  .get('/:provider/callback', async ({ ally, params }) => {
+    const gh = ally.use('github')
+
+    /**
+     * User has denied access by canceling
+     * the login flow
+     */
+    if (gh.accessDenied()) {
+      return 'You have cancelled the login process'
+    }
+
+    /**
+     * OAuth state verification failed. This happens when the
+     * CSRF cookie gets expired.
+     */
+    if (gh.stateMisMatch()) {
+      return 'We are unable to verify the request. Please try again'
+    }
+
+    /**
+     * GitHub responded with some error
+     */
+    if (gh.hasError()) {
+      return gh.getError()
+    }
+
+    /**
+     * Access user info
+     */
+    const user = await gh.user()
+    return user
+  })
+  .where('provider', /github/)
